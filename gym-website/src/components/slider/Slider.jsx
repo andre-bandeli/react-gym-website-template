050e3c4df19de67c309024001bd03e9c74262ca9
andre-bandeli <andre.lbandeli@gmail.com>
2022-07-23 21:25:40 -0300

    feat: campos slider

--- gym-website/src/components/slider/Slider.jsx
+++ gym-website/src/components/slider/Slider.jsx
@@ -4,7 +4,22 @@ import './slider.scss'
 export default function Slider() {
   return (
     <div id='slider'>
-
+        <div className="container">
+            <div className="col-6">
+                <h2>ola</h2>
+            </div>
+            <div className="col-6">
+                <div className="container-md">
+                    <h2>ola</h2>
+                </div>
+                <div className="container-md-2">
+                    <h2>ola</h2>
+                </div>
+                <div className="container-md-3">
+                    <h2>ola</h2>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
