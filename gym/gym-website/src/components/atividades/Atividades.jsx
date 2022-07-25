db8655ba52f86ab8e8eebdc1c4afee867e7ccc4b
andre-bandeli <andre.lbandeli@gmail.com>
2022-07-24 22:02:09 -0300

    feat: integrando react + django

--- gym/gym-website/src/components/atividades/Atividades.jsx
+++ gym/gym-website/src/components/atividades/Atividades.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './atividades.scss'
 
+
 export default function () {
   return (
     <div id='atividades'>
         <div className="container">
           <div className="col-4">
-            <img src="/assets/imagens/atleta.jpg" alt="" />
+            
+            <img src="{% load static 'assets/imagens/atleta.jpg' %}" alt="" />
             <div className="container-dark">
               <h3>Musculação </h3>
             </div>
